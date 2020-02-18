804d28c4ac91c226dbe8940aafe00d5a28a64438
develleoper <develleoper@users.noreply.github.com>
2020-02-18 07:06:30 +0000

    deploy: 0d2d9a9a080dfebcf090f18ff15d4a61198bfdc7

---
+++ precache-manifest.979a612f0ee07cff8aa90951acb95aa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47c6935df672ac3a657be719158d573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67516a0f14887ce06b5b",
+    "url": "/static/css/main.c496c521.chunk.css"
+  },
+  {
+    "revision": "3093b3a804d3a4f3ee64",
+    "url": "/static/js/2.61100829.chunk.js"
+  },
+  {
+    "revision": "67516a0f14887ce06b5b",
+    "url": "/static/js/main.88bb2710.chunk.js"
+  },
+  {
+    "revision": "d80be0ef3d9ccda0ca43",
+    "url": "/static/js/runtime-main.a94b47eb.js"
+  }
+]);
