9fd5ff0d7ea72f3f0d73c90a4bed07e9d30b7abd
Danielle Burgess <danielle.a.burgess@gmail.com>
2020-02-17 23:11:45 -0800

    switch to hashrouter for gh-pages support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import './index.css';
 import Page from './Page';
 import SpreadRoute from './Spread';
@@ -9,12 +9,12 @@ export const AppContext = createContext({})
 
 const App = () =>
   <AppContext.Provider value={useState(AppContext)}>
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Page} />
         <Route path="/spread" component={SpreadRoute} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   </AppContext.Provider>
 
 ReactDOM.render(<App />, window.app);
